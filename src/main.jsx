32cf655cde43111525d8fc519557e615517682f9
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-09-06 20:45:34 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Main extends Component {
+    render() {
+        return (
+            <h1>Hey</h1>
+        )
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
