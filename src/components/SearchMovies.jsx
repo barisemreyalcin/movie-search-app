4f9732ec6fd1d43b34d8689d4e584776bcfea415
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-09-07 13:32:54 +0300

    style the movie cards

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -30,11 +30,11 @@ function SearchMovies() {
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
                     <div className="card" key={movie.id}>
-                        <img className="card--img" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + "poster"}/>
-                        <div className="card-content">
-                            <div className="card-title">{movie.title}</div>
+                            <img className="card--img" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + "poster"}/>
+                        <div className="card--content">
+                            <h3 className="card--title">{movie.title}</h3>
                             <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
+                            <p><small>RATING: {Number(movie.vote_average).toFixed(1)}</small></p>
                             <p className="card-desc">{movie.overview}</p>
                         </div>
                     </div>
